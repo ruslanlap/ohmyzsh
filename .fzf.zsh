090f1ec348f0203ea4ba48c1bb979a95d2c03c00
ruslanlap <lapin@ucu.edu.ua>
2023-03-29 01:10:27 +0300

    oh-my-zsh-settings

---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/manjaro/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/manjaro/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/manjaro/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/manjaro/.fzf/shell/key-bindings.zsh"
